f9c8c6936520c8af00a754d2dcb9aaca0fc50bf7
biulight <biulight007@gmail.com>
2025-01-12 21:57:29 +0800

    fix: the config of baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   title: 'biulight Site',
   tagline: '持续记录学习',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: './',
+  baseUrl: '/blog/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
